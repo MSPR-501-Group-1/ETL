3cb441b1b09620a416fa0239a51dcb9961c32486
3AlblA3 <alexandreblanc22@gmail.com>
2026-04-11 12:00:48 +0200

    clean config files

--- processors/nutrition/config.py
+++ processors/nutrition/config.py
@@ -12,9 +12,6 @@ RAW_DIR.mkdir(parents=True, exist_ok=True)
 PROCESSED_DIR.mkdir(parents=True, exist_ok=True)
 
 KAGGLE_DATASET = "adilshamim8/daily-food-and-nutrition-dataset"
-DATASET_FILENAME = "Nutrition_Dataset.zip"
-
-LOCAL_ZIP = RAW_DIR / DATASET_FILENAME
 LOCAL_FILE = RAW_DIR / "Nutrition_Dataset.csv"
 
 # ── Source 2 : nutritional-values-for-common-foods-and-products ────────────────
@@ -22,9 +19,6 @@ RAW_DIR_2 = ROOT_DIR / "data" / "raw" / "nutrition_values"
 RAW_DIR_2.mkdir(parents=True, exist_ok=True)
 
 KAGGLE_DATASET_2 = "trolukovich/nutritional-values-for-common-foods-and-products"
-DATASET_FILENAME_2 = "Nutrition_values_Dataset.zip"
-
-LOCAL_ZIP_2 = RAW_DIR_2 / DATASET_FILENAME_2
 LOCAL_FILE_2 = RAW_DIR_2 / "Nutrition_values_Dataset.csv"
 
 # Output paths
